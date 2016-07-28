5ab71c7fccfdd5b44817e2b5a375eb7b4a2ecf52
Richard Fox <prime@ahref.co.uk>
2016-07-28 21:42:42 +0100

    Code!

---
+++ test/unit/commands.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+const _ = require('lodash');
+
+const commands = require('../../lib/sysex/command/factory');
+const constants = require('../../lib/sysex/constants');
+
+describe('command compiler', () => {
+  it('creates a command factory for each type of command', () => {
+    var keys = Object.keys(constants.command);
+    var commandKeys = Object.keys(commands);
+    expect(commandKeys).to.deep.equal(keys);
+    expect(_.every(commands, command => typeof command === 'function')).to.be.ok;
+  });
+  it('creates a valid command object when a factory function is used', () => {
+    const command = commands.DETACH_SERVO();
+    expect(command.code).to.equal(constants.command.DETACH_SERVO);
+    expect(command.schema).to.not.be.null;
+  })
+});
---
+++ test/unit/encoderTest.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const encoder = requires('../../lib/sysex/encoder.js');
+
+describe('encoder', () => {
+  it('Encodes Properly',() => {
+  });
+})
